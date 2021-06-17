ad4b9e0ba3045adfb5739379a853692f8896daec
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-06-17 16:41:46 +0100

    EPRS

--- src/main/java/com/payroll/web/Controller/EmployeeController.java
+++ src/main/java/com/payroll/web/Controller/EmployeeController.java
@@ -7,10 +7,16 @@ import com.payroll.web.Exceptions.EmployeeCannotBeNullException;
 import com.payroll.web.Exceptions.EmployeeNotFoundException;
 import com.payroll.web.Exceptions.IdCannotBeNullException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/employee")
@@ -19,24 +25,34 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @GetMapping()
-    public List<Employee> findAll(){
-        return employeeService.findAll();
+    public ResponseEntity<?>findAll(){
+//        EntityModel<Employee>employeeEntityModel=EntityModel
+//        return employeeService.findAll();
+        return null;
     }
     @PostMapping()
-    public Employee save(@Valid @RequestBody EmployeeDto employee ) throws EmployeeCannotBeNullException {
-        return employeeService.save(employee);
+    public  ResponseEntity<?> save(@Valid @RequestBody EmployeeDto employeeDto ) throws EmployeeCannotBeNullException, IdCannotBeNullException, EmployeeNotFoundException {
+        Employee employee= employeeService.save(employeeDto);
+        EntityModel<Employee>employeeEntityModel=EntityModel.of(
+               employee,linkTo(methodOn(EmployeeController.class)
+                        .findById(employee.getId())).
+                        withSelfRel(),linkTo(methodOn(EmployeeController.class).
+                        findAll()).withRel("employees"));
+        return ResponseEntity.created(employeeEntityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(employeeEntityModel);
     }
     @GetMapping("/{id}")
-    public Employee findById(@PathVariable("id") Integer id) throws IdCannotBeNullException, EmployeeNotFoundException {
-        return employeeService.findById(id);
+    public  ResponseEntity<?> findById(@PathVariable("id") Integer id) throws IdCannotBeNullException, EmployeeNotFoundException {
+        employeeService.findById(id);
+        return null;
     }
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable("id") Integer id) throws IdCannotBeNullException, EmployeeNotFoundException {
         employeeService.deleteById(id);
     }
     @PatchMapping("/{id}")
-    public Employee updateEmployee(@PathVariable("id")Integer id,@RequestBody EmployeeDto employeeDto) throws EmployeeNotFoundException {
-        return employeeService.updateEmployee(id,employeeDto);
+    public  ResponseEntity<?> updateEmployee(@PathVariable("id")Integer id,@RequestBody EmployeeDto employeeDto) throws EmployeeNotFoundException {
+        employeeService.updateEmployee(id,employeeDto);
+        return null;
     }
 
 }
